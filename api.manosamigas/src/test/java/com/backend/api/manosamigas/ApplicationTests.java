9b097c1dbf085f91eab296bdb01ad4177f10bd94
Mikel <mikeljcp@gmail.com>
2023-12-02 15:20:43 -0300

    Proyecto iniciado - Backend

---
+++ api.manosamigas/src/test/java/com/backend/api/manosamigas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.api.manosamigas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
